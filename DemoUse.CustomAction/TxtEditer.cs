6cb90a41dfaabee3c189566946759a5c891aa756
Aslan <tom7826@hotmail.com>
2021-08-02 20:10:01 +0800

    @加入Json 與 文字編輯

---
+++ DemoUse.CustomAction/TxtEditer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoUse.CustomAction
+{
+    public class TxtEditer
+    {
+        private string PATH;
+        public TxtEditer(string path)
+        {
+            PATH = path;
+        }
+
+        /// <summary>
+        /// 取得文字檔資料
+        /// </summary>
+        /// <returns></returns>
+        public string GetTxtData()
+        {
+            var result = string.Empty;
+            using (StreamReader reader = new StreamReader(PATH, System.Text.Encoding.Default))
+            {
+                result = reader.ReadToEnd();
+            }
+            return result;
+        }
+        /// <summary>
+        /// 完全覆蓋文字檔
+        /// </summary>
+        /// <returns></returns>
+        public void EditTxtData(string txt)
+        {
+            using (StreamWriter writer = new StreamWriter(PATH, false, System.Text.Encoding.Default))
+            {
+                writer.Write(txt);
+            }
+        }
+    }
+}

3772560d911ad43c0f75812511493d673f86894f
Aslan <tom7826@hotmail.com>
2021-08-02 20:11:01 +0800

    @完成Json編輯撰寫

--- DemoUse.CustomAction/CustomAction.cs
+++ DemoUse.CustomAction/CustomAction.cs
@@ -1,6 +1,9 @@
 using Microsoft.Deployment.WindowsInstaller;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace DemoUse.CustomAction
@@ -10,9 +13,18 @@ namespace DemoUse.CustomAction
         [CustomAction]
         public static ActionResult SetCustomAction(Session session)
         {
+            //可使用的參數
+            var path = Path.Combine(session["INSTALLFOLDER"], "appsettings.json");
+            var systemIP = session["SYSTEMIP"];
+            var webPort = session["WEBPORT"];
            
-
-
+            //編輯Json檔
+            var txtEditer = new TxtEditer(path);
+            string txt = txtEditer.GetTxtData();
+            var config = JsonConvert.DeserializeObject<JObject>(txt);
+            config["URL"] = $"{systemIP}:{webPort}";
+            txt = JsonConvert.SerializeObject(config, Formatting.Indented);
+            txtEditer.EditTxtData(txt);
 
             return ActionResult.Success;
         }

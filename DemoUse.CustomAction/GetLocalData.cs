a4169bf1b99da87612689840bad17f455cb4f735
Aslan <tom7826@hotmail.com>
2021-08-26 20:06:28 +0800

    @GetLocalData 增加GetPathRegistryKey的方式

--- DemoUse.CustomAction/GetLocalData.cs
+++ DemoUse.CustomAction/GetLocalData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -17,6 +18,21 @@ namespace DemoUse.CustomAction
                     .Where(o => o.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork && !o.ToString().EndsWith(".1"))
                     .FirstOrDefault()?.ToString() ?? "127.0.0.1";
             return ip;
+        }
+        /// <summary>
+        /// 取得機碼路徑
+        /// </summary>
+        /// <returns></returns>
+        public string GetPathRegistryKey()
+        {
+            string result = string.Empty;
+            var key = Registry.LocalMachine.OpenSubKey("SOFTWARE\\WOW6432Node\\Demo\\DemoUse.Installer", false)
+                    ?? Registry.LocalMachine.OpenSubKey("SOFTWARE\\Demo\\DemoUse.Installer", false);
+            if (key != null)
+            {
+                result = key.GetValue("InstallDir").ToString();
+            }
+            return result;
         }
     }
 }

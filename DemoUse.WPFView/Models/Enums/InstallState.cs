b58489a0a0e83d30b89694ec2ef09db109acb316
Aslan <tom7826@hotmail.com>
2021-08-26 18:58:47 +0800

    @抽離 InstallState enum

---
+++ DemoUse.WPFView/Models/Enums/InstallState.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoUse.WPFView.Models.Enums
+{
+    /// <summary>
+    /// 執行時的狀態
+    /// </summary>
+    public enum InstallState
+    {
+        /// <summary>
+        /// 開始
+        /// </summary>
+        Initializing,
+        /// <summary>
+        /// 已安裝
+        /// </summary>
+        Present,
+        /// <summary>
+        /// 未安裝
+        /// </summary>
+        NotPresent,
+        /// <summary>
+        /// 進行中
+        /// </summary>
+        Applying,
+        /// <summary>
+        /// 取消
+        /// </summary>
+        Cancelled,
+        /// <summary>
+        /// 進行結束
+        /// </summary>
+        Applied,
+        Failed,
+    }
+}

18408f5bc43a539b5efca8d9a939e1b0eacb5b29
Aslan <tom7826@hotmail.com>
2021-09-13 15:15:50 +0800

    @加入設定頁面

---
+++ DemoUse.WPFView/ViewModels/SetDataPageViewModel.cs
@@ -0,0 +1,89 @@
+﻿using DemoUse.WPFView.Models;
+using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace MaxPower.NetPro.Setup.WPFView.ViewModels
+{
+    public class SetDataPageViewModel : PropertyNotifyBase
+    {
+        internal BootstrapperApplicationModel _model;
+        /// <summary>
+        /// 安裝命令
+        /// </summary>
+        public ICommand InstallCommand { get; private set; }
+        public ICommand SelectedFolderCommand { get; private set; }
+
+        public SetDataPageViewModel(BootstrapperApplicationModel model)
+        {
+            _model = model;
+            InstallFollder = @"C:\Program Files (x86)\DemoUse";
+            InstallCommand = new RelayCommand(param => Install(), param => true);
+            SelectedFolderCommand = new RelayCommand(param => Browse(), param => true);
+        }
+
+        private string _installFollder;
+        public string InstallFollder
+        {
+            get
+            {
+
+                return _installFollder;
+            }
+            set
+            {
+                if (_installFollder != value && ValidDir(value))
+                {
+                    _installFollder = value;
+                    OnPropertyChanged("InstallFollder");
+                }
+            }
+        }
+
+
+        public void Browse()
+        {
+            var folderBrowserDialog = new FolderBrowserDialog { SelectedPath = InstallFollder };
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                InstallFollder = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        /// <summary>
+        /// path是否是正确的文件夹路径
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private bool ValidDir(string path)
+        {
+            try
+            {
+                string p = new DirectoryInfo(path).FullName;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// 關閉事件
+        /// </summary>
+        public Action Close;
+
+        /// <summary>
+        /// 安裝
+        /// </summary>
+        public void Install()
+        {
+            _model.PlanAction(LaunchAction.Install);
+            Close();
+        }
+    }
+}
---
+++ DemoUse.WPFView/Views/SetDataPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using MaxPower.NetPro.Setup.WPFView.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoUse.WPFView.Views
+{
+    /// <summary>
+    /// SetDataPage.xaml 的互動邏輯
+    /// </summary>
+    public partial class SetDataPage : Window
+    {
+        public SetDataPage(SetDataPageViewModel viewModel)
+        {
+            InitializeComponent();
+            this.DataContext = viewModel;
+            viewModel.Close = () => Close();
+        }
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
